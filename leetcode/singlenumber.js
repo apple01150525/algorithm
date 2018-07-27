a2867b82adaf87dae916d0dc33ae84fd918eeb02
guoshuang10 <guoshuang1@jd.com>
2018-07-27 14:30:01 +0800

    bugfix

--- leetcode/singlenumber.js
+++ leetcode/singlenumber.js
@@ -1,7 +1,7 @@
 const arr = [17, 12, 5, -6, 12, 4, 17, -5, 2, -3, 2, 4, 5, 16, -3, -4, 15, 15, -4, -5, -6];
 var singleNumber = function(nums) {
     nums = nums.sort(function(a, b) {
-        return a - b < 0;
+        return a - b;
     });
     console.log(nums);
     for (i = 0; i < nums.length; i = i + 2) {
