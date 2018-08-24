faa52074ad7b4cbfb81cba078a40701c292658c1
apple01150525 <clover532202375@163.com>
2018-08-24 10:16:53 +0800

    add valid-palindrome

---
+++ leetcode/validPalindrome.js
@@ -0,0 +1,9 @@
+// leetcode 125 验证回文串
+var isPalindrome = function(s) {
+    if (s.length == 0) {
+        return true;
+    }
+    var s1 = s.toLocaleLowerCase(); /*    正则替换掉*/
+    var s2 = s1.replace(/\W|_/g, '');
+    return s2.split('').reverse().join('') == s2;
+};
