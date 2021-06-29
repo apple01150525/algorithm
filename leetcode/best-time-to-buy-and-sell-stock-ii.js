18847dd023a9b974690319c58e85081e2d033c13
guoshuang6 <guoshuang6@tal.com>
2021-06-29 22:58:52 +0800

    添加股票买卖时机

---
+++ leetcode/best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,17 @@
+// leetcode 122
+// 买卖股票的最佳时机 II
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    var profit = 0;
+    for(let i = 0; i< prices.length; i++) {
+        var tmp =  prices[i] - prices[i - 1];
+        if(tmp > 0) {
+            profit +=tmp;
+        }
+    }
+    return profit;
+};
